d62603026e25b4df9305ca3188d9094e00e272c9
Júlio Elias de Sousa rocha <julioeliasdesousarocha.work@gmail.com>
2024-05-12 08:57:04 -0300

    Project update

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const path = require('path')
+const session = require('express-session');
+const flash = require('connect-flash');
 
 const app = express();
 const PORT = 8081;
 
 const operations = require("./routes/operations");
+
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/LoginApp")
+     .then(() => {
+          console.log("Conectado ao mongo!");
+     })
+     .catch((error) => {
+          console.log("Erro ao se conectar ao mongo: " + error); 
+     });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -23,10 +34,27 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(session({
+     secret: "qualquercoisa",
+     resave: true,
+     saveUninitialized: true
+}));
+
+app.use(flash());
+app.use((req, res, next) => {
+     res.locals.success_msg = req.flash("success_msg");
+     res.locals.error_msg = req.flash("error_msg");
+     next();
+});
+
 app.get("/", (req, res) => {
-     res.render("register");
+     res.render("operationsPage/login");
+});
+
+app.get("/register", (req, res) => {
+     res.render("operationsPage/register");
 });
 
 app.use("/operations", operations);
 
-app.listen(PORT, () => { console.log(`Rodando na url: https://localhost${PORT}`); });
+app.listen(PORT, () => { console.log(`Rodando na url: https://localhost:${PORT}`); });
--- routes/operations.js
+++ routes/operations.js
@@ -1,6 +1,57 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
 
+const User = require("../models/User");
+
+router.post("/register", (req, res) => {
+     
+     var erros = [];
+     if (!req.body.user || req.body.user == undefined || req.body.user == null) {
+          erros.push({ texto: "Digite um nome de usuário!" });
+     } else {
+          if (req.body.user.length < 3) {
+               erros.push({ texto: "Digite um nome de usuário com mais de 3 caracteres!" });
+          };
+     };
+     if (!req.body.email || req.body.email == undefined || req.body.email == null) {
+          erros.push({ texto: "Digite um email!" });
+     } else {
+          const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+          if (!emailRegex.test(req.body.email)) {
+               erros.push({ texto: "Formato de email inválido!" });
+          };
+     };
+     if (!req.body.password || req.body.password == undefined || req.body.password == null) {
+          erros.push({ texto: "Digite uma senha!" });
+     } else {
+          if (req.body.password.length < 6) {
+               erros.push({ texto: "A senha precisa de no mínimo 6 caracteres!" });
+          };
+     };
+
+     
+     if (erros.length > 0) {
+          res.render("operationsPage/register", { erros: erros });
+     } else {
+          const newUser = {
+               user: req.body.user,
+               email: req.body.email,
+               password: req.body.password,
+          }
+          new User(newUser).save()
+               .then(() => {
+                    console.log("User registered successfully");
+                    req.flash("success_msg", "Usuário criado com sucesso. Faça Login!");
+                    res.redirect("/");
+               })
+               .catch((error) => {
+                    console.log("Error registering user");
+                    req.flash("error_msg", "Houve um erro ao cadastrar usuário. Tente novamente!");
+                    res.redirect("/register");
+               });
+     };
+});
 
 router.post("/login", (req, res) => {
 
